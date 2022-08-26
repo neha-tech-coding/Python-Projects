61506ba1b9f9e80d0e46b602ed2102d87d8e46b5
deepak-padampur <deepaksuna4290@gmail.com>
2022-08-26 18:31:11 +0530

    product model registered

--- server/products/models.py
+++ server/products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+# DB
+# class---> Database Table Name
+# attributes---> Columns
+# instance/object --- row
+
 # Create your models here.
+
+class Product(models.Model):
+    title=models.CharField(max_length=50)  #max length
+    description=models.TextField(null=True) #empty pass
+    price=models.FloatField(default=99)
+    updatedAt=models.DateTimeField(auto_now=True)
+    createdAt=models.DateTimeField(auto_created=True)
+    id=models.IntegerField(primary_key=True)
+
+
