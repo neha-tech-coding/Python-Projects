db55b227272cdfaf3adc65c3f58cbb0e4a43082f
royallieno <gulnawazarshad@gmail.com>
2022-08-28 17:41:17 +0530

    views_name-updated

--- server/products/views.py
+++ server/products/views.py
@@ -4,27 +4,22 @@ import requests
 
 # Create your views here.
 # baseUrl='https://jsonplaceholder.typicode.com/posts'
-products=[{
-    "id":1,"title":"abc"
-},{
-    "id":2,"title":"xyz"
-}]
+products = [{"id": 1, "title": "abc"},
+            {"id": 2, "title": "xyz"}]
 
-def getProducts(request,*args,**kwargs):
-    return render(request,'products/products.html',{"productsList":products,"name":"Arshad"})
 
-def getProductById(request,id):
-    productId=int(id)
+def getProducts(request, *args, **kwargs):
+    return render(request, 'products/products.html', {"productsList": products, "name": "Gulnawaz Arshad"})
+
+
+def getProductById(request, id):
+    productId = int(id)
     for product in products:
-        if product["id"]==productId:
-            content=product
-            return render(request,'products/productDetails.html',{"productDetail":content})
+        if product["id"] == productId:
+            content = product
+            return render(request, 'products/productDetails.html', {"productDetail": content})
 
 
-
-
-
-#
 # def getProducts(request,*args,**kwargs):
 #     posts=requests.get(baseUrl).json()
 #     return JsonResponse({"data":posts,"message":"I am from get products","status":200})
